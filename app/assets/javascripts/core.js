4491d64f805a1a17ae8f49c62d20f967c230b944
Paul DiPietro <paul.dipietro@me.com>
2012-12-19 11:44:18 -0500

    Updated README; minor layout change

---
+++ app/assets/javascripts/core.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  var loadData = function(time_within) {
+    $("#products").html("");
+    _.each(gon.products, function(product) {
+      var productId = "\"product" + gon.products.indexOf(product) + "\"";
+      if (gon.products.indexOf(product) % 2 == 0) {
+        $("#products").append("<div class=\"product even_product\" id=\"product\">⬆ ⬇ " + product.name +
+          " | " + "Total Reviews: " + product.total_reviews + " | " + "Avg. Rating: " + product.average + "</div>");
+      } else {
+        $("#products").append("<div class=\"product odd_product\" id=\"product\">⬆ ⬇ " + product.name +
+          " | " + "Total Reviews: " + product.total_reviews + " | " + "Avg. Rating: " + product.average + "</div>");
+      }
+    });
+  };
+  loadData(7);
+
+  $("#reviews_within").each(function() {
+    $("a").click(function(clicked) {
+      if($(this).hasClass("active")) {
+        $(this).removeClass("active");
+      } else {
+        $(this).addClass("active");
+      }
+      loadData(30);
+    });
+  });
+});
