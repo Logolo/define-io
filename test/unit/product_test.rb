b137678af31d9963531a4d0cfc6a618975dd4ac5
Paul DiPietro <paul.dipietro@me.com>
2013-01-21 14:41:29 -0500

    Added test for Product attributes, fixed date on issues

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -15,4 +15,9 @@ class ProductTest < ActiveSupport::TestCase
     product = Product.new(:name => "Test Product")
     assert !product.save, "Product saved without a description."
   end
+
+  test "product has a both an average and rating sum of 0 by default" do
+    product = Product.new
+    assert product.average && product.rating_sum == 0, "Product's default average/rating_sum values are not 0"
+  end
 end
