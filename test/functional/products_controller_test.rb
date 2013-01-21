ead631df4d4a70495e5526e5610dbd2d97fe779b
Paul DiPietro <paul.dipietro@me.com>
2013-01-21 15:38:48 -0500

    Added Products controller test

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
   # test "the truth" do
   #   assert true
   # end
+  test "should not get new products form if not signed in or admin" do
+    @user = User.new
+    @user.admin = true # enabling or disabling this allows for access
+    sign_in :user, @user
+    sign_in @user
+    get :new
+    assert_response :redirect unless @user.admin?
+  end
 end
