6b69b0999f55e4f515fb96c3c221b34c5ac51aeb
Paul DiPietro <paul.dipietro@me.com>
2013-01-21 11:32:26 -0500

    Writing tests for User model

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -5,8 +5,13 @@ class UserTest < ActiveSupport::TestCase
   #    assert true, "Truth passed."
   #  end
 
-  test "user cannot be created due to lacking information" do
+  test "user cannot be created due to lacking various creation information" do
     user = User.new
     assert !user.save
+  end
+
+  test "user confirmation requires password and password confirmation" do
+    user = User.new
+    assert user.admin == false, "User is admin on creation."
   end
 end
