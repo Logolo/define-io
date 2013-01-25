639e452b5f90b39a4eab6dc898217dca888ff208
Paul DiPietro <paul.dipietro@me.com>
2013-01-24 19:43:24 -0500

    Adding/working on user profile view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @reviews = Review.where("user_id = ?", @user)
     @title = "Profile for " + @user.name
   end
 end
